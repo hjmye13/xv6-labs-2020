5004158934c59918e1d4426d2fd70502947e56af
suirui17 <r13167566588@163.com>
2022-04-29 14:51:47 +0800

    Buffer cache:not tested

--- kernel/bio.c
+++ kernel/bio.c
@@ -23,14 +23,20 @@
 #include "fs.h"
 #include "buf.h"
 
+#define NBUFMAP_BUCKET 13
+#define BUFMAP_HASH(dev, blockno) ((((dev)<<27)|(blockno))%NBUFMAP_BUCKET)
+
 struct {
-  struct spinlock lock;
+  //struct spinlock lock;
   struct buf buf[NBUF];
 
   // Linked list of all buffers, through prev/next.
   // Sorted by how recently the buffer was used.
   // head.next is most recent, head.prev is least.
-  struct buf head;
+  //struct buf head;
+  struct buf bufmap[NBUFMAP_BUCKET]; // 该数组中的buf为dummy head
+  struct spinlock bufmap_locks[NBUFMAP_BUCKET];
+  struct spinlock bufeviction_locks[NBUFMAP_BUCKET];
 } bcache;
 
 // buffer cache是双向链接的列表
@@ -39,11 +45,27 @@ binit(void)
 {
   struct buf *b;
 
-  initlock(&bcache.lock, "bcache");
+  for(int i = 0; i < NBUFMAP_BUCKET; i++) {
+    initlock(&bcache.bufmap_locks[i], "bcache_bufmap");
+    initlock(&bcache.bufeviction_locks[i], "bcache_bufeviction");
+    bcache.bufmap[i].next = 0;
+  }
+
+  for (int i = 0;i < NBUF; i++) {
+    struct buf *temp = &bcache.buf[i];
+    acquire(&bcache.bufmap[0]);
+    temp->lastuse = 0;
+    temp->refcnt = 0;
+    temp->next = bcache.bufmap[0].next;
+    bcache.bufmap[0].next = temp;
+    release(&bcache.bufmap[0]);
+  }
+  //initlock(&bcache.lock, "bcache");
 
   // Create linked list of buffers
-  bcache.head.prev = &bcache.head;
-  bcache.head.next = &bcache.head;
+  //bcache.head.prev = &bcache.head;
+  //bcache.head.next = &bcache.head;
+  /*
   for(b = bcache.buf; b < bcache.buf+NBUF; b++){
     b->next = bcache.head.next;
     b->prev = &bcache.head;
@@ -51,6 +73,7 @@ binit(void)
     bcache.head.next->prev = b;
     bcache.head.next = b;
   }
+  */
   // 将数组buf中的块链接起来，每次都接到head的后面
   // 其余所有对buf的访问都是通过head链表进行的，而不是直接访问buf数组
 }
@@ -64,10 +87,80 @@ static struct buf*
 bget(uint dev, uint blockno)
 {
   struct buf *b;
+  uint key = BUFMAP_HASH(dev, blockno);
 
-  acquire(&bcache.lock);
+  acquire(&bcache.bufmap_locks[key]);
+
+  for (b = bcache.bufmap[key].next; b; b = b->next) {
+    if (b->dev == dev && b->blockno == blockno) {
+      b->refcnt++;
+      release(&bcache.bufmap[key]);
+      acquiresleep(&b->lock); // 确保同一时间只有一个进程使用该缓存块
+    }
+  }
+
+  release(&bcache.bufmap_locks[key]);
+  acquire(&bcache.bufeviction_locks);
+
+  for (b = bcache.bufmap[key].next; b; b = b->next) {
+    if (b->dev == dev && b->blockno == blockno) {
+      acquire(&bcache.bufmap_locks);
+      b->refcnt++;
+      release(&bcache.bufmap_locks[key]);
+      release(&bcache.bufeviction_locks[key]);
+      acquiresleep(&b->lock); // 确保同一时间只有一个进程使用该缓存块
+    }
+  }
+
+  struct buf *before_last = 0;
+  uint holding_bucket = -1;
+
+  for (int i = 0; i < NBUFMAP_BUCKET; i++) {
+    acquire(&bcache.bufmap_locks[i]);
+
+    int new_buf = 0;
+    for (b = &bcache.bufmap[i]; b->next; b = b->next) {
+      if (b->next->refcnt == 0 && ((!before_last) || b->lastuse < before_last->next->lastuse)) {
+        before_last = b;
+        new_buf = 1;
+      }
+    }
+    if (!new_buf) {
+      release(&bcache.bufmap_locks[i]);
+    } else {
+      if (holding_bucket != -1) {
+        release(&bcache.bufmap_locks[holding_bucket]);
+      }
+      holding_bucket = i;
+    }
+  }
+
+  if (!before_last) {
+    panic("bget: no buffers");
+  }
+
+  b = before_last->next;
+  if (holding_bucket != key) {
+    before_last->next = b->next; // 从原来的bucket中移除
+    release(&bcache.bufmap_locks[holding_bucket]);
+    acquire(&bcache.bufmap_locks[key]);
+    b->next = bcache.bufmap[key].next;
+    bcache.bufmap[key].next = b;
+  }
+
+  b->dev = dev;
+  b->blockno = blockno;
+  b->refcnt = 1;
+  b->valid = 0;
+  release(&bcache.bufmap_locks[key]);
+  release(&bcache.bufeviction_locks[key]);
+  acquiresleep(&b->lock);
+  return b;
+  
+  //acquire(&bcache.lock);
 
   // Is the block already cached?
+  /*
   for(b = bcache.head.next; b != &bcache.head; b = b->next){
     if(b->dev == dev && b->blockno == blockno){
       b->refcnt++; // 递增引用次数
@@ -92,6 +185,7 @@ bget(uint dev, uint blockno)
     }
   }
   panic("bget: no buffers");
+  */
   // 没有可分配的buffer
 }
 
@@ -128,34 +222,33 @@ brelse(struct buf *b)
 
   releasesleep(&b->lock); // 将blocked和pid置零，并且唤醒等待该锁的进程
 
-  acquire(&bcache.lock);
+  uint key = BUFMAP_HASH(b->dev, b->blockno);
+
+  acquire(&bcache.bufmap_locks[key]);
   b->refcnt--;
   if (b->refcnt == 0) {
     // 没有进程使用当前buf，将其移到buf list的头部
     // no one is waiting for it.
-    b->next->prev = b->prev;
-    b->prev->next = b->next;
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
+    b->lastuse = ticks;
   }
   
-  release(&bcache.lock);
+  release(&bcache.bufmap_locks[key]);
 }
 
 void
 bpin(struct buf *b) {
-  acquire(&bcache.lock);
+  uint key = BUFMAP_HASH(b->dev, b->blockno);
+  acquire(&bcache.bufmap_locks[key]);
   b->refcnt++;
-  release(&bcache.lock);
+  release(&bcache.bufmap_locks[key]);
 }
 
 void
 bunpin(struct buf *b) {
-  acquire(&bcache.lock);
+  uint key = BUFMAP_HASH(b->dev, b->blockno);
+  acquire(&bcache.bufmap_locks[key]);
   b->refcnt--;
-  release(&bcache.lock);
+  release(&bcache.bufmap_locks[key]);
 }
 
 
