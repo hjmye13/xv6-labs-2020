822798dc30cbb6a2c9ec317d9b5b41ebc1d766b2
hujiamiao <hujiamiao@eswincomputing.com>
2024-11-19 11:55:05 +0800

    kernel tbl per proc

--- kernel/defs.h
+++ kernel/defs.h
@@ -108,6 +108,8 @@ void            yield(void);
 int             either_copyout(int user_dst, uint64 dst, void *src, uint64 len);
 int             either_copyin(void *dst, int user_src, uint64 src, uint64 len);
 void            procdump(void);
+void
+proc_freekernelpt(pagetable_t kernelpt);
 
 // swtch.S
 void            swtch(struct context*, struct context*);
@@ -180,6 +182,10 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+pagetable_t     proc_kpt_init(void); // 用于内核页表的初始化
+void            proc_inithart(pagetable_t kpt);
+void
+uvmmap(pagetable_t pagetable, uint64 va, uint64 pa, uint64 sz, int perm);
 
 // plic.c
 void            plicinit(void);
